27746c19d0e8529f8a272937f9c1054ce0c897a0
kanix1506 <kanittha_thaotan@cmu.ac.th>
2025-12-23 15:01:17 +0700

    lab9_commit

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int M,int N){
+	if(M <= 0 or N <= 0){
+		cout << "Invalid input"<< "\n";
+	}
+	for(int x = 0;x<M;x++){
+		for(int p = 0;p<N;p++){
+			cout<<"O";
+		}
+		cout<<"\n";
+	}
+}
 
 int main(){
 	
